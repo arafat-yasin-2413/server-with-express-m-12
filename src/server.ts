185bd16352e8cd93458beba01b282768e09a83d2
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-11 11:26:33 +0600

    installed and configured dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), '.env')});
 
 const app = express();
 const port = 5000;
@@ -10,7 +14,7 @@ app.use(express.json());
 
 // database connection
 const pool = new Pool({
-	connectionString: `postgresql://neondb_owner:npg_db1hvA6UaQDV@ep-long-forest-a4wc2y0u-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+	connectionString: `${process.env.CONNECTION_STRING}`,
 });
 
 const initDB = async () => {
