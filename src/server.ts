a8c21ebdf5e012335d91412121d7488431f7c228
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-11 11:01:24 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+
+const app = express();
+const port = 5000;
+
+// parser (middleware)
+app.use(express.json());
+// app.use(express.urlencoded());
+
+// database connection
+const pool = new Pool({
+	connectionString: `postgresql://neondb_owner:npg_db1hvA6UaQDV@ep-long-forest-a4wc2y0u-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
+
+const initDB = async () => {
+	await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL,
+                age INT,
+                phone VARCHAR(15),
+                address TEXT,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+        `);
+};
+
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello Worldddd!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+	console.log(req);
+
+	res.status(201).json({
+		success: true,
+		message: "API is Working",
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
