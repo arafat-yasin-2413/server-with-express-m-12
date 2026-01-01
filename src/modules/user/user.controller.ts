875f8566d4cce7d199ad9c6d5279c835735f45fc
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-01 19:44:17 +0600

    user controller created to get all users

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -21,8 +21,28 @@ const createUser = async (req: Request, res: Response) => {
             message: err.message,
         });
     }
+};
+
+const getAllUser = async (req: Request, res: Response) => {
+    try {
+        const result = await userServices.getAllUser();
+        // console.log("All users :", result);
+
+        res.status(200).json({
+            success: true,
+            message: "Data retrieved Successfully",
+            data: result.rows,
+        });
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err,
+        });
+    }
 }
 
 export const userControllers = {
     createUser,
+    getAllUser,
 }
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -7,26 +7,6 @@ const router = express.Router();
 router.post("/", userControllers.createUser );
 
 
-router.get("/", async (req: Request, res: Response) => {
-    try {
-        const result = await pool.query(
-            `
-            SELECT * FROM users`
-        );
-        // console.log("All users :", result);
-
-        res.status(200).json({
-            success: true,
-            message: "Data retrieved Successfully",
-            data: result.rows,
-        });
-    } catch (err: any) {
-        res.status(500).json({
-            success: false,
-            message: err.message,
-            details: err,
-        });
-    }
-});
+router.get("/", userControllers.getAllUser);
 
 export const userRoutes = router;
