6f736a7b4fac2cb59cd0bbde9047c650c538abc2
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-02 11:35:53 +0600

    auth route added in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import initDB, { pool } from "./config/db";
 import logger from "./middleware/logger";
 import { userRoutes } from "./modules/user/user.routes";
 import { todoRoutes } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
 
 
 const app = express();
@@ -27,6 +28,8 @@ app.get("/", logger, (req: Request, res: Response) => {
 app.use("/users",userRoutes)
 app.use("/todos",todoRoutes);
 
+// auth routes
+app.use("/auth",authRoutes);
 
 // Not Found route
 app.use((req, res) => {
