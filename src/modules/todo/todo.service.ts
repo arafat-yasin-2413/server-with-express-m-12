b9c9cd7ca1f5d66233e7edde5d19817b62f0c837
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-02 08:41:58 +0600

    users schema modified

--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -1,38 +1,41 @@
 import { pool } from "../../config/db";
 
-
-const createTodo = async (payload: Record <string,unknown>) =>{
-    const {user_id, title} = payload;
-    const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING * `,[user_id, title]);
-    return result;
+const createTodo = async (payload: Record<string, unknown>) => {
+	const { user_id, title } = payload;
+	const result = await pool.query(
+		`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING * `,
+		[user_id, title]
+	);
+	return result;
 };
 
-const getAllTodo = async()=>{
-    const result = await pool.query(`SELECT * FROM todos`);
-    return result;
-}
+const getAllTodo = async () => {
+	const result = await pool.query(`SELECT * FROM todos`);
+	return result;
+};
 
-const getTodoById = async (id:string) =>{
-    const result = await pool.query(`SELECT * FROM todos WHERE id = $1`,[id]);
-    return result;
-}
+const getTodoById = async (id: string) => {
+	const result = await pool.query(`SELECT * FROM todos WHERE id = $1`, [id]);
+	return result;
+};
 
-const updateTodoById = async (title:string, completed:boolean, id:string) =>{
-    const result = await pool.query(`UPDATE todos SET title=$1, completed=$2 WHERE id=$3 RETURNING *`,[title, completed, id]);
-    return result;
-}
+const updateTodoById = async (title: string, completed: boolean, id: string) => {
+	const result = await pool.query(
+		`UPDATE todos SET title=$1, completed=$2 WHERE id=$3 RETURNING *`,
+		[title, completed, id]
+	);
+	return result;
+};
 
-const deleteTodoById = async (id:string) =>{
-    const result = await pool.query(`DELETE FROM todos WHERE id=$1 RETURNING *`,[id]);
-    return result;
-}
-
-
+const deleteTodoById = async (id: string) => {
+	const result = await pool.query(`DELETE FROM todos WHERE id=$1 RETURNING *`, [id]);
+	return result;
+};
 
 export const todoServices = {
-    createTodo,
-    getAllTodo,
-    getTodoById,
-    updateTodoById,
-    deleteTodoById,
-}
+	createTodo,
+	getAllTodo,
+	getTodoById,
+	updateTodoById,
+	deleteTodoById,
+};
