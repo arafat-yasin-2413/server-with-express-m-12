cc5b32142d80edc2246d8b39af897b5fb6810486
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-03 11:18:30 +0600

    namespace created

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,27 @@
 // This is auth middleware
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 // higher order function
 const auth = () => {
-	return (req: Request, res: Response, next: NextFunction) => {
-		const token = req.headers.authorization?.split(" ")[1];
-		console.log("------ printing from auth middleware file --------");
-		if (!token) {
-			return res.status(500).json({ message: "You are not allowed" });
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const token = req.headers.authorization?.split(" ")[1];
+			console.log("------ printing from auth middleware file --------");
+			if (!token) {
+				return res.status(500).json({ message: "You are not allowed" });
+			}
+			console.log({ authToken: token });
+			const decoded = jwt.verify(token, config.jwtSecret as string);
+			console.log({ decoded });
+			req.user = decoded as JwtPayload;
+			return next();
+		} catch (err: any) {
+			res.status(500).json({
+				success: false,
+				message: err.message,
+			});
 		}
-		console.log({ authToken: token });
-		const decoded = jwt.verify(token, config.jwtSecret as string);
-		console.log({ decoded });
-		return next();
 	};
 };
 
